b3841d01b5c3a4c3f758bee0c5f090730782c330
kai.draw <hainh12@techcombank.com.vn>
2026-03-19 11:35:20 +0700

    Release v1.0.3 (build 4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "com.snap.tiles"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildTypes {
